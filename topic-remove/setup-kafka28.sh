0de9a6c082a433fd7547c3840c32c5378d7854cb
fabiojose <fabiojose@gmail.com>
2021-06-29 06:44:43 -0300

    Testar setup por script background
    Signed-off-by: fabiojose <fabiojose@gmail.com>

---
+++ topic-remove/setup-kafka28.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export KAFKA_HOME=/root/kafka
+export PATH=$PATH:$KAFKA_HOME/bin
+
+# Download
+curl 'https://downloads.apache.org/kafka/2.8.0/kafka_2.12-2.8.0.tgz' -o kafka.tgz
+
+# Extract
+tar -xvzf kafka.tgz
+
+# Start zookeeper
+zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties&
+sleep 5
+
+# Start Broker
+kafka-server-start.sh $KAFKA_HOME/config/server.properties \
+--override broker.id=1 \
+--override log.dirs=/tmp/broker1-logs \
+--override listeners=PLAINTEXT://:9092 \
+--override zookeeper.connect=localhost:2181 \
+--override zookeeper.connection.timeout.ms=10000
